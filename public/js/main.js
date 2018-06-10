6c5ef0d9559eaf82b86dc1f8db8f261327fcb8ab
Bjørn Karmann <bjoern.t.karmann@gmail.com>
2018-06-10 20:49:14 +0200

    dependencies clean-up
    removed wav-encoder and sound-analyser

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect('http://localhost:8000');
 import * as Canvas from './canvas.js';
 socket.emit('msg', 'train');
 // resive data from server
--- server/js/sound.js
+++ server/js/sound.js
@@ -39,19 +39,20 @@ function sound() {
   var fs = require('fs');
   var mic = require('mic'); //https://github.com/ashishbajaj99/mic
   //Decoder dependecies
-  var header = require("waveheader"); https:// https://www.npmjs.com/package/waveheader
-  var WavDecoder = require('wav-decoder'); // https://www.npmjs.com/package/wav-decoder
+  var header = require("waveheader"); //https://www.npmjs.com/package/waveheader
   //For calculations
   var math = require('mathjs');
-  //mic config object
-
+  // wav file encoder
   var wav = require('node-wav');
+
+  //mic config object
   const config = {
-    rate: 4096, // 4096 44100 2048 4000
+    rate: 16000, // 4096 44100 2048 4000
     channels: 2,
     debug: true
   };
-  var size = 1024; // size to slice
+
+  var size = 512; // size to slice
 
   this.startRecord = function(callback){
     var buffers = [];
@@ -59,27 +60,21 @@ function sound() {
     var stream = micInstance.getAudioStream();
 
     stream.on('data', buffer => {
+      if(config.debug) console.time("bufferTime");
       buffers.push(buffer); // -> save previous recorded data
       var headerBuf = header(config.rate, config); // ->  create wav header
       buffers.unshift(headerBuf); // -> set header in top of buffers
       var length = math.sum(buffers.map(b => b.length));
       try{
         var result = wav.decode(Buffer.concat(buffers, length)) // -> decode buffers to float array
-        var wave = result.channelData[0]; // sample rate is 44100
-
-        //   rate 44100 -> 11025 wav length (only 5512) -> 2048 FFT (4096 slice)
-        //   rate 16000 -> 4000 wav length (full)       -> 1024 FFT (2048 slice)
-        //   rate 8000  -> 2000 wav length (full)       -> need more numbers
-        //   rate 8000  -> 2000 wav length (sliced)     -> 512 FFT (1024 slice)
-        // * rate 8192  -> 2048 wav length (only 1024)  -> 512 FFT (1024 slice)
-        //   rate 4096  -> 1024 wav length (full)       -> 512 FFT (gives errors)
-
-        //console.log(wave);
-
+        var wave = result.channelData[0];
         var slice = wave.slice(0,size);
         var fftValues = makeFFT(slice);
-        console.log(fftValues);
-        // callback(fftValues);
+        callback(fftValues);
+        if(config.debug) {
+          console.timeEnd("bufferTime");
+          console.log("Max: "+math.max(fftValues));
+        }
       }catch(err){
         console.error(err);
       }
@@ -103,10 +98,12 @@ function sound() {
     //Convert from Object to Array
     var arr = [];
     var i = 0;
+
     Object.keys(spectrum).map(function(key){
-      arr[i] = Math.round(spectrum[key]*100, 4);
+      arr[i] = math.round(spectrum[key]*100, 4);
       i++;
     })
+    //console.log("Max: "+math.max(arr));
     return arr; // --> Return array of FFT values
   }
 }
