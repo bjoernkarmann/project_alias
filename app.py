96e5db257e083ef757e8ff87b1983a96fd827278
Tore Knudsen <toreknudsn@gmail.com>
2019-05-02 21:48:15 +0200

    Update kws on server side, added noise + timer

--- app.py
+++ app.py
@@ -24,23 +24,12 @@ import os
 # style app
 # built in recomendation (random on plus )
 
-# add timer
 # send result back to app
-# update speech object
 
 # sd card image (try again)
 # new readme (update instructables)
 
-# Global inits
-#====================================================#
-#init and setup RPI LEDs
-led.LED.off()
-#Initialize the sound objects
-noise = sound.audioPlayer("data/noise.wav",-1,"noise",True)
-#wakeup = sound.audioPlayer("data/ok_google2.wav",0,"wakeup", False)
 
-globals.initialize()
-settings.read() # load settings from json file and save in globals
 
 # Socket connection between client
 #====================================================#
@@ -53,8 +42,7 @@ def incoming(message):
         print('settings update recieved')
         settings.write(newSetting)
         settings.updateKeyphrase(newSetting)
-        # at this point we want to reset the speech model or reload kws
-        #os.execl(sys.executable, os.path.abspath(__file__), *sys.argv) //Reset program
+        globals.CONFIG_HAS_CHANGED = True
 
     if('request' in msg):
         # send settings when client requests them
@@ -64,52 +52,81 @@ def incoming(message):
 # End of socket
 #====================================================#
 
+def speechInit():
+    print("init live speech")
+    return LiveSpeech(
+        audio_device = None,
+        sampling_rate=16000,
+        lm=False,
+        kws='data/keyphrase.list')
+
+def socket_thread():
+    print("starting socket thread")
+    connect.socketio.run(connect.app, host=connect.HOST, port=connect.PORT, debug=False, log_output=False)
+
+def speech_thread():
+    # when a keyphrase is detected, the for loop runs (LiveSpeech magic)
+    for phrase in globals.SPEECH:
+        topWord = phrase.segments()[0]
+        print(topWord)
+        lookup = globals.SETTING['setting'] # get setting
+        # lookup the setting words
+        led.LED.on()
+        for data in lookup:
+            # check the topword with setting list
+            if data['name'].lower().strip() == topWord.lower().strip():
+                # get the whisper command matching the word
+                noise.stop()
+                print('say:', data['whisper'])
+                sound.speak(data['whisper'])
+        time.sleep(2) # delay for 2 second
+        led.LED.off()
+        noise.play()
 
 
+def main():
+    # Global inits
+    #====================================================#
 
-print("init live speech")
-speech = LiveSpeech(
-    audio_device = None,
-    sampling_rate=16000,
-    lm=False,
-    kws='data/keyphrase.list')
+    #Initialize the sound objects
+    globals.initialize()
+    settings.read() # load settings from json file and save in globals
 
-def main_thread():
-    print("starting thread")
+    noise.play()
+    #init and setup RPI LEDs
+    led.LED.off()
+    globals.SPEECH = speechInit()
+
+    thread_speech = Thread(target=speech_thread)
+    thread_speech.daemon = True
+    thread_speech.start()
+
+    thread = Thread(target=socket_thread)
+    thread.daemon = True
+    thread.start()
+
+
+    print('')
+    print("============================================")
+    print("SERVER RUNNING ON: http://" + str(connect.HOST) + ":" + str(connect.PORT))
+    print("============================================")
+    print('')
+
     while True:
-        # when a keyphrase is detected, the for loop runs (LiveSpeech magic)
-        for phrase in speech:
-            topWord = phrase.segments()[0]
-            print(topWord)
-            lookup = globals.SETTING['setting'] # get setting
-            # lookup the setting words
-            led.LED.on()
-            for data in lookup:
-                # check the topword with setting list
-                if data['name'].lower().strip() == topWord.lower().strip():
-                    # get the whisper command matching the word
-                    # Stop noise
-                    print('say:', data['whisper'])
-                    sound.speak(data['whisper'])
-            time.sleep(1) # delay for 1 second
-            led.LED.off()
-            # Start noise
+        time.sleep(0.3)
+        print("loop")
 
-thread = Thread(target=main_thread)
-thread.daemon = True
-thread.start()
+        #reset program when config is changed
+        if globals.CONFIG_HAS_CHANGED:
+            globals.CONFIG_HAS_CHANGED = False
+            main()
 
-noise.play()
-
-print('')
-print("============================================")
-print("SERVER RUNNING ON: http://" + str(connect.HOST) + ":" + str(connect.PORT))
-print("============================================")
-print('')
 
 # Start socket io
 if __name__ == '__main__':
-    connect.socketio.run(connect.app, host=connect.HOST, port=connect.PORT, debug=False, log_output=False)
+     noise = sound.audioPlayer("data/noise.wav")
+     main()
+
 
 def exit_handler():
     led.LED.off()
@@ -117,5 +134,6 @@ def exit_handler():
     sound.mixer.stop()
     os.system("ps -fA | grep python")
     thread._stop()
+    thread_speech.stop()
 
 atexit.register(exit_handler)
--- modules/globals.py
+++ modules/globals.py
@@ -1,19 +1,9 @@
 def initialize():
-    global SETTING, RESULT, BG_EXAMPLES, TR_EXAMPLES, TRAIN, PREDICT, RESET, SILENCE, SPECTOGRAM_FULL, MIC_TRIGGER, EXAMPLE_READY, UPDATE_BG_DATA, BUTTON_PRESSED,HAS_BEEN_TRAINED, TRIGGERED
+    global SETTING, RESET, SILENCE,TRIGGERED, SPEECH, CONFIG_HAS_CHANGED
 
     SETTING     = []
     RESET       = False
-    TRAIN       = False
-    PREDICT     = True
-    RESULT      = 3
-    BG_EXAMPLES = 0
-    TR_EXAMPLES = 0
     SILENCE		= False
-    SPECTOGRAM_FULL = False
-    SPECTOGRAM_FULL_FFT = False
-    MIC_TRIGGER = False
-    EXAMPLE_READY = False
-    UPDATE_BG_DATA = False # Set true to record new examples to background data set
-    BUTTON_PRESSED = False
-    HAS_BEEN_TRAINED = False
     TRIGGERED = False
+    SPEECH = 0
+    CONFIG_HAS_CHANGED = False
