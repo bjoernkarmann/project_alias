670ebd69b930f211a3f453334b4cec94b0a5c7cb
Bjørn Karmann <bjornkarmann@Bjrns-MacBook-Pro.local>
2020-01-05 20:15:51 +0100

    Licence updates

--- modules/settings.py
+++ modules/settings.py
@@ -1,3 +1,21 @@
+"""
+Project Alias - Rename your assistant and makes sure it never listens.
+Copyright (C) 2020  Bjørn Karmann & Tore Knudsen
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
 import json
 from modules import globals
 
@@ -15,8 +33,8 @@ def read():
 # get setting and write names into keyphrase.list file
 def updateServer(data):
     print(data)
-    #sensitivity = data['setting']['sensitivity'] # 0 - 10 
-    
+    #sensitivity = data['setting']['sensitivity'] # 0 - 10
+
     with open(globals.KEYWORD_PATH, 'w') as f:
         for item in data['keyphrase']:
             #threshold = int(mapF(len(item['name']),4, 15, 0 , 30)) + int(sensitivity)
